045afe3783eaf2aa84f12fc39f32bfeeffcc8ea6
heinburger <steven.c.hein@gmail.com>
2018-11-21 18:29:44 +0100

    Updates

---
+++ precache-manifest.3d25cd763cbbc15c1ed18029af07a7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dff20d8ce4c8b72a2f49951f66895e5b",
+    "url": "/voice-to-text-stream/static/media/logo.dff20d8c.svg"
+  },
+  {
+    "revision": "74f9d599d163001657de",
+    "url": "/voice-to-text-stream/static/js/runtime~main.74f9d599.js"
+  },
+  {
+    "revision": "9466914df297dd9c670f",
+    "url": "/voice-to-text-stream/static/js/main.9466914d.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/voice-to-text-stream/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "9466914df297dd9c670f",
+    "url": "/voice-to-text-stream/static/css/main.7604f7ec.chunk.css"
+  },
+  {
+    "revision": "1d282fd15651be521d658b8fcb245b1c",
+    "url": "/voice-to-text-stream/index.html"
+  }
+];
