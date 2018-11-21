12234620d21c8fc685d5973663b289877b5ccd48
heinburger <steven.c.hein@gmail.com>
2018-11-21 21:56:01 +0100

    Add guess

--- src/VoiceOutput.js
+++ src/VoiceOutput.js
@@ -3,10 +3,10 @@ import React, { Component } from 'react';
 class VoiceOutput extends Component {
   render() {
     return (
-      <div style={{ padding: '15px', flex: '1 1 auto', display: 'flex', height: '100%' }}>
-        <div style={{ color: 'gray' }}>{ this.props.guess }</div>
+      <div style={{ padding: '15px', flex: '1 1 auto', display: 'flex', height: '100%', flexDirection: 'column' }}>
+        <div style={{ color: 'gray', flex: '0 0 auto' }}>{ this.props.guess || '...'}</div>
         <textarea
-          style={{ fontSize: '24px', width: '100%', height: '100%' }}
+          style={{ fontSize: '24px', width: '100%', flex: '1 1 auto' }}
           value={ this.props.output }
           onChange={ () => false } />
 
