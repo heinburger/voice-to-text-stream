0a9e20cabf137c17afe6e13ec0386bc4943d700d
Steven Hein <steven.c.hein@gmail.com>
2018-11-29 19:56:41 +0100

    Updates

---
+++ precache-manifest.53f2f2c03a2b6e8c6064b727f4b8923c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dff20d8ce4c8b72a2f49951f66895e5b",
+    "url": "/voice-to-text-stream/static/media/logo.dff20d8c.svg"
+  },
+  {
+    "revision": "74f9d599d163001657de",
+    "url": "/voice-to-text-stream/static/js/runtime~main.74f9d599.js"
+  },
+  {
+    "revision": "422fa206768416f4d0ca",
+    "url": "/voice-to-text-stream/static/js/main.422fa206.chunk.js"
+  },
+  {
+    "revision": "e490b339e298faf3afa0",
+    "url": "/voice-to-text-stream/static/js/1.e490b339.chunk.js"
+  },
+  {
+    "revision": "422fa206768416f4d0ca",
+    "url": "/voice-to-text-stream/static/css/main.8543d070.chunk.css"
+  },
+  {
+    "revision": "6a8ef3540e7735009034974f9b1f87b0",
+    "url": "/voice-to-text-stream/index.html"
+  }
+];
